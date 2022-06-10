ac9d394a2b9ca5b2634f8c3444673bdbdd188e3a
shaikhmudassir <55383416+shaikhmudassir@users.noreply.github.com>
2022-06-10 11:35:33 +0530

    Add files via upload

---
+++ html/php_arithmatic.php
@@ -0,0 +1,14 @@
+<?php
+    $n1 = 50;
+    $n2 = 5;
+
+    $add = $n1 + $n2;
+    $sub = $n1 - $n2;
+    $mul = $n1 * $n2;
+    $div = $n1 / $n2;
+
+    echo "Addition " . $add . "<br>"
+        ."Substraction " . $sub . "<br>"
+        ."Multiplication" . $mul . "<br>"
+        ."Division" . $div . "<br>";
+?>
